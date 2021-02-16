29ab8a6ebf56e03ae26852aef9dcfe7d44d55a61
artevance <agustinus.michael29@gmail.com>
2021-02-16 16:15:36 +0700

    Make repository and view abstractions

---
+++ app/Abstracts/Database/Repository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Abstracts\Database;
+
+use Closure;
+use Illuminate\Support\Facades\DB;
+
+abstract class Repository
+{
+    /**
+     * @param  \Closure
+     * @return mixed
+     */
+    protected function transaction(Closure $closure)
+    {
+        return DB::transaction($closure);
+    }
+}
---
+++ app/Abstracts/Database/View.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Abstracts\Database;
+
+abstract class View
+{
+    
+}
