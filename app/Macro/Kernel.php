3738f5f382815939168b21ec214a0b5a6014fd50
artevance <agustinus.michael29@gmail.com>
2021-02-16 10:17:37 +0700

    Make macro kernel

---
+++ app/Macro/Kernel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Macro;
+
+class Kernel
+{
+    /**
+     * @var array
+     */
+    public static $database = [
+
+        /**
+         * Blueprints.
+         */
+        'blueprints' => [
+            
+        ]
+    ];
+}
