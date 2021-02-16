3e00357f163d9977418110402ec1114141f13b56
artevance <agustinus.michael29@gmail.com>
2021-02-16 16:12:59 +0700

    Make transformer abstraction

---
+++ app/Abstracts/Transformers/Resources/RelatedResource.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Abstracts\Transformers\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+abstract class RelatedResource extends JsonResource {  }
---
+++ app/Abstracts/Transformers/Resources/SingleResource.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Abstracts\Transformers\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+abstract class SingleResource extends JsonResource {  }
