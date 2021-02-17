c7c7ba18f54d9b25c2c5b6e6a56c14ede430aea1
artevance <agustinus.michael29@gmail.com>
2021-02-17 12:23:05 +0700

    Make authorized form request

---
+++ app/Abstracts/Http/AuthorizedFormRequest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Abstracts\Http;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Auth\Access\AuthorizationException;
+
+class AuthorizedFormRequest extends FormRequest
+{
+    /**
+     * Default unauthorized message.
+     * 
+     * @var string
+     */
+    protected $unauthorizedMessage = 'Anda tidak memiliki otoritas';
+    
+    /**
+     * Automatically authorize every request.
+     * 
+     * @return bool
+     */
+    public function authorize()
+    {
+        return with(new Permission($this, $this->moduleRef, $this->actionRef))
+            ->verify()
+            ->log()
+            ->result();
+    }
+
+    /**
+     * Customize the Failed Authorization method with custom message.
+     * 
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     * @return void
+     */
+    protected function failedAuthorization()
+    {
+        throw new AuthorizationException($this->unauthorizedMessage);
+    }
+
+    /**
+     * Return a new rule object for validation.
+     * 
+     * @param  string  $ruleDottedNamespace
+     * @return \Illuminate\Contracts\Validation\Rule
+     */
+    protected function ruleObject($ruleDottedNamespace, ...$args)
+    {
+        $ruleObject = $this->getRuleClassName($ruleDottedNamespace);
+
+        return new $ruleObject(...$args);
+    }
+
+    /**
+     * Override the get validator instance to enable after validation
+     * hook.
+     * 
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function getValidatorInstance()
+    {
+        $validator = parent::getValidatorInstance();
+        $validator->after(fn ($validator) => $this->afterValidation($validator));
+        return $validator;
+    }
+
+    /**
+     * Master after validation method.
+     * 
+     * @return void
+     */
+    protected function afterValidation() {   }
+
+    /**
+     * Get the rule class name based on dotted, snake case name.
+     * 
+     * @param  string  $ruleDottedNamespace
+     * @return string
+     */
+    private function getRuleClassName($ruleDottedNamespace)
+    {
+        $explodedRuleDottedNamespace = explode('.', $ruleDottedNamespace);
+
+        return Str::snakeDotToPascalBackslash($ruleDottedNamespace,
+            fn ($ruleNamespace) => '\App\Http\Validation\Rules\\'.$ruleNamespace
+        );
+    }
+}
