82b840f1434735fa791a03c829022d1a813f021a
artevance <agustinus.michael29@gmail.com>
2021-02-18 11:42:53 +0700

    Relocate the vue application dirs from the old repository

---
+++ resources/js/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container-fluid">
+        <router-view />
+        <div class="app-version-wrapper">
+            {{ bangsusSistemVersionComputed }}
+        </div>
+        <div class="screen-mode-wrapper">
+            <template v-if="$isMobile()">
+                Mode Mobile
+            </template>
+            <template v-else>
+                Mode Desktop
+            </template>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        bangsusSistemVersion: {
+            type: String,
+            required: true,
+        },
+        laravelVersion: {
+            type: String,
+            required: true,
+        },
+        phpVersion: {
+            type: String,
+            required: true,
+        },
+    },
+    computed: {
+        bangsusSistemVersionComputed() {
+            return 'v' + this.bangsusSistemVersion
+        },
+        laravelVersionComputed() {
+            return 'v' + this.laravelVersion
+        },
+        phpVersionComputed() {
+            return 'v' + this.phpVersion
+        },
+    },
+}
+</script>
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-window._ = require('lodash');
+window.lodash = require('lodash');
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
