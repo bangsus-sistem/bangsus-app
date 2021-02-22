2053e748b898c8a7eece7837694ac901f74b5098
artevance <agustinus.michael29@gmail.com>
2021-02-22 10:25:22 +0700

    Add abstract method of manage to service

--- app/Abstracts/Http/Service.php
+++ app/Abstracts/Http/Service.php
@@ -2,7 +2,13 @@
 
 namespace App\Abstracts\Http;
 
+use Illuminate\Http\Request;
+
 abstract class Service
 {
-    
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    abstract public function manage($request);
 }
