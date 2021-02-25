89c5c890ee4bc8d91d5557b9d0fbfbe1381f57df
artevance <agustinus.michael29@gmail.com>
2021-02-25 09:24:32 +0700

    Make api token model and migration

---
+++ app/Database/Models/Auth/ApiToken.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Database\Models\Auth;
+
+use Illuminate\Database\Eloquent\{
+    Model,
+    SoftDeletes,
+};
+use App\Database\Models\Concerns\Relations\HasUserTimestamps;
+use App\Database\Models\Concerns\Timestamp\{
+    ExpiredAtTimestamp,
+    UsedAtTimestamp,
+};
+
+class ApiToken extends Model
+{
+    use SoftDeletes;
+
+    use HasUserTimestamps;
+
+    use ExpiredAtTimestamp, UsedAtTimestamp;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function user()
+    {
+        return $this->hasMany(\App\Database\Models\Auth\User::class);
+    }
+}
