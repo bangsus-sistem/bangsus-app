e1a6aeef2ac8d81653ca9f004acb2e1d6b3e4557
artevance <agustinus.michael29@gmail.com>
2021-02-25 08:42:24 +0700

    Make lang resources

---
+++ resources/lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Password anda sudah direset!',
+    'sent' => 'Link reset password sudah kami kirimkan ke email!',
+    'throttled' => 'Tunggu beberapa saat sebelum mencoba lagi.',
+    'token' => 'Token tidak valid.',
+    'user' => "User tidak ditemukan.",
+
+];
